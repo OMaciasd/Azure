cacd2c75bc5758f609b4c5952c2f054037b6d84b
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-05-02 19:20:52 -0500

    :bug: - Update azure.sh

--- sops/azure.sh
+++ sops/azure.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 az login \
-    && export AZURE_CLIENT_ID="30d4352e-e699-4b37-975d-6e38d30a30f6"
-        export AZURE_CLIENT_SECRET="f3Z8Q~.PdkQ1Mg-9ZMTRZFFdSBtBgx6y-uwscaGp"
-        export AZURE_TENANT_ID="141d8fe2-bf63-4de7-8782-1e1b896f03c9"
+    && export AZURE_CLIENT_ID="A2uR3-(|13n7-1D"
+        export AZURE_CLIENT_SECRET="A2uR3-(|13n7--53(R37"
+        export AZURE_TENANT_ID="A2uR3--73n4N7-1D"
 
 clear
 
